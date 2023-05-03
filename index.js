78188d16cb1f93699b4e08d55295e09743cb4f09
Павел <58857483+Haskiro@users.noreply.github.com>
2023-05-03 21:53:40 +0300

    middlewares

--- index.js
+++ index.js
@@ -8,8 +8,19 @@ app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
 });
 
+app.use((req, res, next) => {
+  console.log("Время", Date.now());
+  next();
+});
+
 app.use("/users", userRouter);
 
 app.get("/hello", (req, res) => {
   res.send("Привет");
+  //   throw new Error("Error");
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).send(err.message);
 });
